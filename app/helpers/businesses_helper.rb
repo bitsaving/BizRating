3dcc613c4b396937918370062f53844dc1012ff8
paras_saini <paras.saini1992@gmail.com>
2015-01-29 09:24:13 +0530

    BR-6 UI Fixes

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -2,4 +2,12 @@ module BusinessesHelper
   def step(step_input)
     "step#{ step_input }"
   end
+
+  def add_error_class(field_name)
+    @business.errors[field_name].empty? ? '' : 'has-error'
+  end
+
+  def error_placeholder_text(field_name)
+    @business.errors[field_name].empty? ? '' : @business.errors[field_name].to_sentence
+  end
 end

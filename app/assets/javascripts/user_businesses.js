80e83809dc4af76ad6d83b1bf8a1f5db4112792e
paras_saini <paras.saini1992@gmail.com>
2015-02-19 16:15:05 +0530

    review controller and ui BR-27

--- app/assets/javascripts/user_businesses.js
+++ app/assets/javascripts/user_businesses.js
@@ -3,13 +3,15 @@ function UserBusinesses (input) {
   this.labels = input.labels;
   this.reviewBtn = input.reviewBtn;
   this.reviewForm = input.reviewForm;
+  this.reviewDetails = input.reviewDetails;
 }
 
 UserBusinesses.prototype.initialize = function() {
   this.labels.first().css({ visibility: 'collapse' });
   this.sliders.next('div').hide();
-  // this.sliders.last().next('div').show().siblings('img').toggleClass('rotate90');
+  this.sliders.last().next('div').show().end().prev('img').toggleClass('rotate90');
   this.reviewForm.hide();
+  this.reviewDetails.hide();
   this.bindEvents();
 };
 
@@ -22,31 +24,39 @@ UserBusinesses.prototype.bindEvents = function() {
 
   this.reviewBtn.on('click', function() {
     _this.reviewForm.show();
-  })
+  });
+
+  this.reviewForm.find("input[type='submit']").on('click', function(e) {
+    if (confirm('Do you want to post this review?')) {
+      return true;
+    } else {
+      e.preventDefault();
+      _this.reviewForm.find('textarea').focus();
+    }
+  });
 
   this.labels.on("click", function() {
     $(this).parent('li')
       .addClass("add-color").prevAll('li').addClass("add-color").end()
       .nextAll('li').removeClass("add-color").end()
       .siblings('input').checked = true;
-  });
-
-  $("ul.rating-star li").hover(function(){
-    $(this).find("label").trigger('click');
+    console.log($(this).parent('li').index());
+    _this.reviewDetails.hide();
+    _this.reviewDetails.eq($(this).parent('li').index()).show();
   });
 
   $(document).on("ajax:success", function(e, data) {
-    $("div.jumbotron").last().append($("<div>", {
+    $("div.jumbotron").last().prepend($("<div>", {
       class: "review "})
       .append("<p><strong>" + data[1] + "&nbsp</strong>"+ data[2] + "</p>")
       .append("<p>" + data[0] + "</p>"));
     $("form.creation").hide();
-    $(".rating div.filled").css({ width: data[3] + "%" });
+    $(".rating div.filled").first().css({ width: data[3] + "%" });
   });
 
   $(document).on("ajax:error", function(e, data) {
-    console.log(data);
     alert(data.responseText);
+    _this.reviewForm.find('textarea').focus();
   });
 };
 
@@ -55,7 +65,8 @@ $(function() {
     sliders: $('.sliders'),
     labels: $("ul.rating-star li label"),
     reviewBtn: $('#new-review-btn'),
-    reviewForm: $('#new-review')
+    reviewForm: $('#new-review'),
+    reviewDetails: $('#rate-detail span')
   },
     userBusiness = new UserBusinesses(input);
   userBusiness.initialize();

144b6a35e755486d62ab10352925d3f884b32f1a
paras_saini <paras.saini1992@gmail.com>
2015-02-23 14:34:03 +0530

    mailer subject change BR-30

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'paras1992testt@gmail.com'
+  config.mailer_sender = 'Biz rating'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'

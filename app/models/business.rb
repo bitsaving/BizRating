3222f0111da46cad385a03e5c3af8bbb3f3e40a0
paras_saini <paras.saini1992@gmail.com>
2015-02-23 18:03:24 +0530

    rate is business button

--- app/models/business.rb
+++ app/models/business.rb
@@ -113,7 +113,7 @@ class Business < ActiveRecord::Base
   end
 
   def update_percentage_star_rating
-    update_column(:percentage_star_rating, Hash[reviews.group(:rating).average(:rating).map{ |k,v| [k, v.to_i * 20] }])
+    update_column(:percentage_star_rating, Hash[reviews.group(:rating).count(:rating).map{ |k,v| [k, v.to_i * 20 / reviews.count] }]) if reviews.present?
   end
 
 end

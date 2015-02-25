1e0a391e924f5f4c8da223627eb180f322cd8c8d
paras_saini <paras.saini1992@gmail.com>
2015-02-25 14:19:27 +0530

    add to_f to script

--- scripts/set_up_average_rating_and_percentage_star_rating_in_business.rb
+++ scripts/set_up_average_rating_and_percentage_star_rating_in_business.rb
@@ -2,7 +2,7 @@ require '../config/environment.rb'
 
 puts 'Updating average_rating'
 Business.find_each do |business|
-  business.update(average_rating: business.reviews.average(:rating))
+  business.update(average_rating: business.reviews.average(:rating).to_f)
 end
 
 puts 'Updating percentage_star_rating'

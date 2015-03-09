6a60d199429e7e7d74d095f78697f7cd26816be1
paras_saini <paras.saini1992@gmail.com>
2015-03-09 19:12:17 +0530

    remove script errors

--- scripts/migrate_from_address_to_latitude_and_longitude.rb
+++ scripts/migrate_from_address_to_latitude_and_longitude.rb
@@ -3,12 +3,16 @@ require 'net/http'
 
 puts 'transforming address to latitude and longitude'
 Address.find_each do |address|
-  uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=" + address.area + "&key=AIzaSyDsihJMaLwegcz19Wfle-gDtTyAeBmaLK4")
+  uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=" + address.area.to_s + "&key=AIzaSyDsihJMaLwegcz19Wfle-gDtTyAeBmaLK4")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  p response.body
-  p ActiveSupport::JSON.decode(response.body).['results'][0]['geometry']['location']
+  response = ActiveSupport::JSON.decode(http.request(request).body)
+  p response['results']
+  if response['results'].empty?
+    p [0,0]
+  else
+    p response['results'][0]['geometry']['location']
+  end
 end

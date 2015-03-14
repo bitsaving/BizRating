54d6884e56ea5608b0585d87b402fe9b0edcd0ed
paras_saini <paras.saini1992@gmail.com>
2015-03-14 12:48:14 +0530

    create manifest file for user_category javascript BR-39

--- app/assets/javascripts/user_category.js
+++ app/assets/javascripts/user_category.js
@@ -1,20 +1,7 @@
 function UserCategory(input) {
-  this.gmap_div = input.gmap_div
-  this.businessLocationList = input.businessLocationList;
   this.selectField = input.selectField;
   this.form = input.form;
 }
-
-UserCategory.prototype.initialize = function() {
-  console.log(this.gmap_div);
-  this.gmap = new google.maps.Map(this.gmap_div[0],
-    { zoom: 16,
-      center: new google.maps.LatLng(this.getCookie('lat'), this.getCookie('lng'))
-    }
-  );
-  this.showMarker();
-  this.bindEvents();
-};
 
 UserCategory.prototype.bindEvent = function() {
   var _this = this;
@@ -23,54 +10,10 @@ UserCategory.prototype.bindEvent = function() {
   });
 };
 
-UserCategory.prototype.showMarker = function() {
-  var _this = this;
-  this.businessLocationList.each(function(index, value) {
-    var location = _this.geoLocation(value),
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(location.lat, location.lng),
-        map: _this.gmap,
-        visible: true,
-      }),
-      info = new google.maps.InfoWindow({
-        content: _this.infoString(value)
-      });
-    google.maps.event.addListener(marker, 'click', function() {
-      info.open(_this.gmap, marker);
-      _this.businessLocationList.eq(index).toggleClass('marker')
-    });
-  });
-};
-
-UserCategory.prototype.infoString = function(element) {
-  return $(element).attr('data-name')
-}
-
-UserCategory.prototype.geoLocation = function(element) {
-  var input = {
-    lat: $(element).attr('data-latitude'),
-    lng: $(element).attr('data-longitude')
-  };
-  return input
-};
-
-UserCategory.prototype.getCookie = function(cookieName) {
-    var name = cookieName + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-};
-
 $(function() {
   var input = {
-    gmap_div : $('#gmap'),
-    businessLocationList : $('#business_list div.business'),
     selectField : $('#order'),
     form: $('#order_form')
   }, userCategory = new UserCategory(input);
-  userCategory.initialize();
+  userCategory.bindEvent();
 })
